dd8c9496acc48394c4093eecebf08b876710a211
Ofcuba3 <100317332+Ofcuba3@users.noreply.github.com>
2022-03-19 18:04:33 -0400

    Update MoodleClient.py

--- MoodleClient.py
+++ MoodleClient.py
@@ -11,7 +11,7 @@ class MoodleClient(object):
         self.username = user
         self.password = passw
         self.session = requests.Session()
-        self.path = 'https://uvs.ucm.cmw.sld.cu/'
+        self.path = 'https://aulacened.uci.cu/'
         self.userdata = None
         self.userid = ''
 
